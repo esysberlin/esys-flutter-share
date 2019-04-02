842bfdb6e6d463dd5744bfdb809f31ab3da6127a
Daniel Wolf <daniel-wolf@outlook.com>
2019-04-02 09:24:54 +0200

    ios platform update

--- ios/Classes/SwiftEsysFlutterSharePlugin.swift
+++ ios/Classes/SwiftEsysFlutterSharePlugin.swift
@@ -10,31 +10,30 @@ public class SwiftEsysFlutterSharePlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        if(call.method == "shareImage"){
-            self.shareImage(arguments: call.arguments)
-        } else if(call.method == "shareText"){
-            self.shareText(arguments: call.arguments)
-        } else {
-            result(FlutterMethodNotImplemented)
+        if(call.method == "text"){
+             self.text(arguments: call.arguments)
+        }
+        if(call.method == "file"){
+            self.file(arguments: call.arguments)
+        }
+        if(call.method == "files"){
+            self.files(arguments: call.arguments)
         }
     }
     
-    func shareImage(arguments:Any?) -> Void {
+    func file(arguments:Any?) -> Void {
         // prepare method channel args
-        let argsMap = arguments as! NSDictionary
-        let fileName:String = argsMap.value(forKey: "fileName") as! String
-        
         // no use in ios
         //let title:String = argsMap.value(forKey: "title") as! String
+        let argsMap = arguments as! NSDictionary
+        let name:String = argsMap.value(forKey: "name") as! String
         
-        // load the iage
+        // load the file
         let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!;
-        let imagePath = NSURL(fileURLWithPath: docsPath).appendingPathComponent(fileName)
-        let imageData:NSData? = NSData(contentsOf: imagePath!)
-        let imageToShare:UIImage = UIImage(data: imageData! as Data)!
+        let contentUri = NSURL(fileURLWithPath: docsPath).appendingPathComponent(name)
         
         // set up activity view controller
-        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: [imageToShare], applicationActivities: nil)
+        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: [contentUri!], applicationActivities: nil)
         
         // present the view controller
         let controller = UIApplication.shared.keyWindow!.rootViewController as! FlutterViewController
@@ -43,13 +42,37 @@ public class SwiftEsysFlutterSharePlugin: NSObject, FlutterPlugin {
         controller.show(activityViewController, sender: self)
     }
     
-    func shareText(arguments:Any?) -> Void {
+    func files(arguments:Any?) -> Void {
         // prepare method channel args
-        let argsMap = arguments as! NSDictionary
-        let text:String = argsMap.value(forKey: "text") as! String
-        
         // no use in ios
         //let title:String = argsMap.value(forKey: "title") as! String
+        let argsMap = arguments as! NSDictionary
+        let names:[String] = argsMap.value(forKey: "names") as! [String]
+        
+        var contentUris:[URL] = [];
+        
+        // load the files
+        for name in names {
+            let docsPath:String = NSSearchPathForDirectoriesInDomains(.cachesDirectory,.userDomainMask , true).first!;
+            contentUris.append(NSURL(fileURLWithPath: docsPath).appendingPathComponent(name)!);
+        }
+        
+        // set up activity view controller
+        let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: contentUris, applicationActivities: nil)
+        
+        // present the view controller
+        let controller = UIApplication.shared.keyWindow!.rootViewController as! FlutterViewController
+        activityViewController.popoverPresentationController?.sourceView = controller.view
+        
+        controller.show(activityViewController, sender: self)
+    }
+    
+    func text(arguments:Any?) -> Void {
+        // prepare method channel args
+        // no use in ios
+        // let title:String = argsMap.value(forKey: "title") as! String
+        let argsMap = arguments as! NSDictionary
+        let text:String = argsMap.value(forKey: "text") as! String
         
         // set up activity view controller
         let activityViewController:UIActivityViewController = UIActivityViewController(activityItems: [text], applicationActivities: nil)
