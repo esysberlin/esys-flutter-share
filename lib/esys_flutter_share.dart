d35ae04cf53c9f41353f1a4ab723c35523c0b1c1
Neckaros <arnaud@jezequel.org>
2021-03-09 15:38:01 +0100

    null safety

--- lib/esys_flutter_share.dart
+++ lib/esys_flutter_share.dart
@@ -11,20 +11,20 @@ class Share {
 
   /// Sends a text to other apps.
   static void text(String title, String text, String mimeType,
-      {Rect sharePositionOrigin}) {
+      {Rect? sharePositionOrigin}) {
     Map argsMap = <String, dynamic>{
       'title': '$title',
       'text': '$text',
       'mimeType': '$mimeType'
     };
-    argsMap = _addRect(argsMap, sharePositionOrigin);
+    argsMap = _addRect(argsMap as Map<String, dynamic>, sharePositionOrigin);
     _channel.invokeMethod('text', argsMap);
   }
 
   /// Sends a file to other apps.
   static Future<void> file(
       String title, String name, List<int> bytes, String mimeType,
-      {String text = '', Rect sharePositionOrigin}) async {
+      {String text = '', Rect? sharePositionOrigin}) async {
     Map argsMap = <String, dynamic>{
       'title': '$title',
       'name': '$name',
@@ -35,14 +35,14 @@ class Share {
     final tempDir = await getTemporaryDirectory();
     final file = await new File('${tempDir.path}/$name').create();
     await file.writeAsBytes(bytes);
-    argsMap = _addRect(argsMap, sharePositionOrigin);
+    argsMap = _addRect(argsMap as Map<String, dynamic>, sharePositionOrigin);
     _channel.invokeMethod('file', argsMap);
   }
 
   /// Sends multiple files to other apps.
   static Future<void> files(
       String title, Map<String, List<int>> files, String mimeType,
-      {String text = '', Rect sharePositionOrigin}) async {
+      {String text = '', Rect? sharePositionOrigin}) async {
     Map argsMap = <String, dynamic>{
       'title': '$title',
       'names': files.entries.toList().map((x) => x.key).toList(),
@@ -57,13 +57,13 @@ class Share {
       await file.writeAsBytes(entry.value);
     }
 
-    argsMap = _addRect(argsMap, sharePositionOrigin);
+    argsMap = _addRect(argsMap as Map<String, dynamic>, sharePositionOrigin);
 
     _channel.invokeMethod('files', argsMap);
   }
 
   static Map<String, dynamic> _addRect(
-      Map<String, dynamic> argsMap, Rect sharePositionOrigin) {
+      Map<String, dynamic> argsMap, Rect? sharePositionOrigin) {
     if (sharePositionOrigin != null) {
       argsMap['originX'] = sharePositionOrigin.left;
       argsMap['originY'] = sharePositionOrigin.top;
