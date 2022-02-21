e9ef2a7922021ff091a976bd93aac02cb6953201
greymag <greymag@gmail.com>
2022-02-21 11:34:34 +0300

    Refactor

--- lib/esys_flutter_share.dart
+++ lib/esys_flutter_share.dart
@@ -20,7 +20,8 @@ class Share {
 
   /// Sends a file to other apps.
   static Future<void> file(
-      String title, String name, List<int> bytes, String mimeType, {String text = ''}) async {
+      String title, String name, List<int> bytes, String mimeType,
+      {String text = ''}) async {
     Map argsMap = <String, String>{
       'title': '$title',
       'name': '$name',
@@ -37,7 +38,8 @@ class Share {
 
   /// Sends multiple files to other apps.
   static Future<void> files(
-      String title, Map<String, List<int>> files, String mimeType, {String text = ''}) async {
+      String title, Map<String, List<int>> files, String mimeType,
+      {String text = ''}) async {
     Map argsMap = <String, dynamic>{
       'title': '$title',
       'names': files.entries.toList().map((x) => x.key).toList(),
